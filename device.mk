b35626701525864799c2cbf29b13cb6af5d5310b
Jenhao Chen <jenhaochen@google.com>
2019-03-20 14:57:51 -0700

    sweet: Enable zram writeback
    Bug: 128889899
    Test: Boot to home
    
    Change-Id: I48ae06e54a66a7d6965afbe8db3d2f157cf8a1f4
    Signed-off-by: GrettoRosella <rezarifaldy12@gmail.com>

--- device.mk
+++ device.mk
@@ -363,4 +363,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.sweet
 
+# ZRAM writeback
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.periodic_wb_delay_hours=24
+
 include vendor/xiaomi/sweet/sweet-vendor.mk
