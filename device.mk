0649ccda036918ce3e978ab12f25ec3d6530c18b
ZeeLog <Danjel.Zammit@gmail.com>
2020-12-19 23:34:15 +0100

    sweet: Build android.hardware.health@2.1-impl.recovery package

--- device.mk
+++ device.mk
@@ -325,6 +325,7 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-impl
 
 # ContextHub
